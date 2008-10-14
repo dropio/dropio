1753055104eabe4a786d4dd9a679099e71aded95
Peter Jaros <peeja@dropio.com>
2008-10-14 15:31:06 -0400

    Starting to spec Dropio and Drops.

---
+++ spec/dropio/drop_spec.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + '/../spec_helper.rb'
+
+include Dropio
+
+describe Drop do
+  
+end
---
+++ spec/dropio_spec.rb
@@ -0,0 +1,8 @@
+require File.dirname(__FILE__) + 'spec_helper.rb'
+
+describe Dropio do
+  it "should store the API key" do
+    Dropio.api_key = "83a05513ddddb73e75c9d8146c115f7fd8e90de6"
+    Dropio.api_key.should == "83a05513ddddb73e75c9d8146c115f7fd8e90de6"
+  end
+end
