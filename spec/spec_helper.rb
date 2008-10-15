1055c7ebf435e77a2908ae21efc001d1beb57784
Peter Jaros <peeja@dropio.com>
2008-10-15 18:17:06 -0400

    Spec Mapper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,15 @@ $:.unshift lib_dir unless $:.include?(lib_dir)
 
 require 'dropio'
 include Dropio
+
+module Enumerable
+  # Apply an expectation to each object in the collection.
+  def each_should(*args)
+    each {|item| item.should(*args)}
+  end
+  
+  # Apply a negative expectation to each object in the collection.
+  def each_should_not(*args)
+    each {|item| item.should_not(*args)}
+  end
+end
