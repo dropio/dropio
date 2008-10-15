f060ae1961796e990d502bbd1e6f57d66c625d3e
Peter Jaros <peeja@dropio.com>
2008-10-15 15:45:09 -0400

    Update Client spec.

--- spec/dropio/client_spec.rb
+++ spec/dropio/client_spec.rb
@@ -12,7 +12,7 @@ describe Client do
     Net::HTTP.should_receive(:get_response).
               with(URI("http://api.drop.io/drops/mydrop?api_key=43myapikey13&token=93mydroptoken97&format=json")).
               and_return(api_response)
-    Client::Mapper.should_receive(:map_drop).with(api_response_body).and_return(mydrop)
+    Client::Mapper.should_receive(:map_drops).with(api_response_body).and_return(mydrop)
     
     Client.instance.find_drop("mydrop", "93mydroptoken97").should == mydrop
   end
