cd37ea2b9a7bf6171b5bcfcd97be0525b236e15b
Peter Jaros <peeja@dropio.com>
2008-10-16 13:31:12 -0400

    Respec Comment and Asset.

--- lib/dropio/asset.rb
+++ lib/dropio/asset.rb
@@ -3,42 +3,55 @@ class Dropio::Asset < Dropio::Model
   attr_accessor :drop, :name, :type, :title, :description, :filesize, :created_at,
                 :thumbnail, :status, :file, :converted, :hidden_url, :pages,
                 :duration, :artist, :track_title, :height, :width, :contents, :url
-                
+  
+  # Returns the comments on this asset.  Comments are loaded lazily.  The first
+  # call to +comments+ will fetch the comments from the server.  They are then
+  # cached until the asset is reloaded.
   def comments
-    @comments = Dropio::Client.instance.find_comments(self) if @comments.nil?
+    @comments = Client.instance.find_comments(self) if @comments.nil?
     @comments ||= []
   end         
   
   def find(drop, name)
-    Dropio::Client.instance.find_asset(drop, name)
+    Client.instance.find_asset(drop, name)
   end
   
+  # Adds a comment to the asset with the given +contents+.  Returns the
+  # new +Comment+.
   def create_comment(contents)
-    Dropio::Client.instance.create_comment(self, contents)
+    Client.instance.create_comment(self, contents)
   end
   
+  # Saves the asset back to drop.io.
   def save
-    Dropio::Client.instance.save_asset(self)
+    Client.instance.save_asset(self)
   end
   
-  def destroy
-    Dropio::Client.instance.destroy_asset(self)
+  # Destroys the asset on drop.io.  Don't try to use an Asset after destroying it.
+  def destroy!
+    Client.instance.destroy_asset(self)
+    nil
   end
   
+  # Returns true if the Asset can be faxed.
   def faxable?
     return type == "Document"
   end
-                
+  
+  # Fax the asset to the given +fax_number+.  Make sure the Asset is +faxable?+
+  # first, or +send_to_fax+ will raise an error.
   def send_to_fax(fax_number)
-    Dropio::Client.instance.send_to_fax(fax_number)
+    raise "Can't fax Asset: #{self.inspect} is not faxable" unless faxable?
+    Client.instance.send_to_fax(fax_number)
+    nil
   end
   
   def send_to_emails(emails = {}, message = nil)
-    Dropio::Client.instance.send_to_email(emails, message)
+    Client.instance.send_to_email(emails, message)
   end
   
   def send_to_drop(drop_name)
-    Dropio::Client.instance.send_to_drop(drop_name)
+    Client.instance.send_to_drop(drop_name)
   end
   
 end
--- spec/dropio/comment_spec.rb
+++ spec/dropio/comment_spec.rb
@@ -1,7 +1,23 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Comment do
+  before(:each) do
+    @comment = Comment.new
+    @client = stub(Client)
+    Client.stub!(:instance).and_return(@client)
+  end
+  
   it "should have the attributes of an Comment" do
-    Comment.new.should respond_to(:id, :contents, :created_at)
+    @comment.should respond_to(:id, :contents, :created_at)
+  end
+  
+  it "should save itself" do
+    @client.should_receive(:save_comment).with(@comment)
+    @comment.save
+  end
+  
+  it "should destroy itself" do
+    @client.should_receive(:destroy_comment).with(@comment)
+    @comment.destroy
   end
 end
