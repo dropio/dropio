531bfe9c36eafa03f8f1f98acde78a1748ec8052
Peter Jaros <peeja@dropio.com>
2008-11-03 18:23:03 -0500

    Dropio::Client should create drops and it should create notes.

--- spec/dropio/client_spec.rb
+++ spec/dropio/client_spec.rb
@@ -1,10 +1,11 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Client do
-  def mock_http(method, path, response)
-    request_klass = {:get => Net::HTTP::Get}[method]
+  def mock_http(method, path, response, form_data = nil)
+    request_klass = {:get => Net::HTTP::Get, :post => Net::HTTP::Post}[method]
     request = mock(request_klass)
-    request_klass.stub!(:new).with(path, an_instance_of(Hash)).and_return(request)
+    request.should_receive(:set_form_data).with(form_data) if form_data
+    request_klass.stub!(:new).with(path, Client::DEFAULT_HEADER).and_return(request)
     
     http = mock(Net::HTTP)
     Net::HTTP.stub!(:new).with("api.drop.io").and_return(http)
@@ -13,10 +14,12 @@ describe Client do
   end
   
   before(:each) do
-    # Don't allow Net::HTTP::Get's to be created without being
+    # Don't allow HTTPRequests to be created without being
     # specifically stubbed, typically with mock_http above.
-    Net::HTTP::Get.stub!(:new).with do |*args|
-      raise "Created an unexpected Net::HTTP::Get!\nNet::HTTP::Get.new(#{args.map { |e| e.inspect }.join(", ")})"
+    [Net::HTTP::Get, Net::HTTP::Post].each do |request_klass|
+      request_klass.stub!(:new).with do |*args|
+        raise "Created an unexpected #{request_klass}!\n#{request_klass}.new(#{args.map { |e| e.inspect }.join(", ")})"
+      end
     end
     
     Dropio.api_url = "http://api.drop.io"
@@ -26,10 +29,36 @@ describe Client do
     @api_response      = stub(Net::HTTPSuccess, :body => @api_response_body)
 
     @mydrop  = stub(Drop, :name => 'mydrop', :admin_token => '93mydroptoken97')
+    @note    = stub(Asset)
     @asset   = stub(Asset, :name => 'some_video', :drop => @mydrop)
     @comment = stub(Comment)
   end
-
+  
+  it "should create drops" do
+    settings = {:name               => "mynewdrop",
+                :admin_password     => "niftieradminpassword",
+                :password           => "niftyguestpassword",
+                :guests_can_comment => true,
+                :guests_can_add     => true,
+                :guests_can_delete  => false,
+                :expiration_length  => "1_WEEK_FROM_NOW",
+                :premium_code       => "yeswecan"}
+    
+    mock_http(:post, "/drops/", @api_response, settings.merge(:api_key => "43myapikey13", :format => "json"))
+    Client::Mapper.should_receive(:map_drops).with(@api_response_body).and_return(@mydrop)
+    Client.instance.create_drop(settings).should == @mydrop
+  end
+  
+  it "should create notes" do
+    mock_http(:post, "/drops/mydrop/assets/", @api_response, :title    => "Just a Note",
+                                                             :contents => "This is just to say",
+                                                             :token    => "93mydroptoken97",
+                                                             :api_key  => "43myapikey13",
+                                                             :format   => "json")
+    Client::Mapper.should_receive(:map_assets).with(@mydrop, @api_response_body).and_return(@note)
+    Client.instance.create_note(@mydrop, "Just a Note", "This is just to say").should == @note
+  end
+  
   it "should find drops" do
     mock_http(:get, "/drops/mydrop?api_key=43myapikey13&token=93mydroptoken97&format=json", @api_response)
     Client::Mapper.should_receive(:map_drops).with(@api_response_body).and_return(@mydrop)
@@ -41,7 +70,7 @@ describe Client do
     Client::Mapper.should_receive(:map_assets).with(@mydrop, @api_response_body).and_return([@asset])
     Client.instance.find_assets(@mydrop).should == [@asset]
   end
-
+  
   it "should find comments" do
     mock_http(:get, %r|^/drops/mydrop/assets/some_video/comments/\?api_key=43myapikey13&token=93mydroptoken97&format=json|, @api_response)
     Client::Mapper.should_receive(:map_comments).with(@asset, @api_response_body).and_return([@comment])
