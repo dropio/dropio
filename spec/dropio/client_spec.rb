e301cadde83d1917155a7916072891ebc53547c5
Peter Jaros <peeja@dropio.com>
2008-11-05 15:37:36 -0500

    Client should save note assets and link assets, plus some stub refactoring.

--- spec/dropio/client_spec.rb
+++ spec/dropio/client_spec.rb
@@ -16,6 +16,13 @@ describe Client do
     http.stub!(:request).with(request).and_return(response)
   end
   
+  def stub_asset(more_stubs={})
+    stub Asset, [:drop, :name, :type, :title, :description, :filesize, :created_at,
+                 :thumbnail, :status, :file, :converted, :hidden_url, :pages,
+                 :duration, :artist, :track_title, :height, :width, :contents, :url].
+                   inject({}) { |stubs, key| stubs[key] = nil; stubs }.merge(more_stubs)
+  end
+  
   before(:each) do
     # Don't allow HTTPRequests to be created without being
     # specifically stubbed, typically with mock_http above.
@@ -32,7 +39,9 @@ describe Client do
     @api_response      = stub(Net::HTTPSuccess, :body => @api_response_body)
 
     @mydrop  = stub(Drop, :name => 'mydrop', :admin_token => '93mydroptoken97')
-    @asset   = stub(Asset, :name => 'some-video', :drop => @mydrop, :url => nil, :contents => nil)
+    @note    = stub_asset(:drop => @mydrop, :name => 'a-note', :contents => "My thoughts on life.")
+    @link    = stub_asset(:drop => @mydrop, :name => 'a-link', :url => "http://google.com/")
+    @file_asset   = stub_asset(:drop => @mydrop, :name => 'some-video')
     @comment = stub(Comment)
   end
   
@@ -52,28 +61,26 @@ describe Client do
   end
   
   it "should create notes" do
-    note = stub(Asset)
     mock_http(:post, "/drops/mydrop/assets/", @api_response, :title    => "Just a Note",
                                                              :contents => "This is just to say",
                                                              :token    => "93mydroptoken97",
                                                              :api_key  => "43myapikey13",
                                                              :format   => "json",
                                                              :version  => "1.0")
-    Client::Mapper.stub!(:map_assets).with(@mydrop, @api_response_body).and_return(note)
-    Client.instance.create_note(@mydrop, "Just a Note", "This is just to say").should == note
+    Client::Mapper.stub!(:map_assets).with(@mydrop, @api_response_body).and_return(@note)
+    Client.instance.create_note(@mydrop, "Just a Note", "This is just to say").should == @note
   end
   
   it "should create links" do
-    link = stub(Asset)
-    mock_http(:post, "/drops/mydrop/assets/", @api_response, :url      => "http://drop.io/",
-                                                             :title    => "Drop.io",
-                                                             :contents => "An awesome sharing site.",
-                                                             :token    => "93mydroptoken97",
-                                                             :api_key  => "43myapikey13",
-                                                             :format   => "json",
-                                                             :version  => "1.0")
-    Client::Mapper.stub!(:map_assets).with(@mydrop, @api_response_body).and_return(link)
-    Client.instance.create_link(@mydrop, "http://drop.io/", "Drop.io", "An awesome sharing site.").should == link
+    mock_http(:post, "/drops/mydrop/assets/", @api_response, :url         => "http://drop.io/",
+                                                             :title       => "Drop.io",
+                                                             :description => "An awesome sharing site.",
+                                                             :token       => "93mydroptoken97",
+                                                             :api_key     => "43myapikey13",
+                                                             :format      => "json",
+                                                             :version     => "1.0")
+    Client::Mapper.stub!(:map_assets).with(@mydrop, @api_response_body).and_return(@link)
+    Client.instance.create_link(@mydrop, "http://drop.io/", "Drop.io", "An awesome sharing site.").should == @link
   end
   
   it "should create comments" do
@@ -84,8 +91,8 @@ describe Client do
                                                                   :api_key  => "43myapikey13",
                                                                   :format   => "json",
                                                                   :version  => "1.0")
-    Client::Mapper.stub!(:map_comments).with(@asset, @api_response_body).and_return(comment)
-    Client.instance.create_comment(@asset, "What a cool video!").should == comment
+    Client::Mapper.stub!(:map_comments).with(@file_asset, @api_response_body).and_return(comment)
+    Client.instance.create_comment(@file_asset, "What a cool video!").should == comment
   end
   
   it "should find drops" do
@@ -96,14 +103,14 @@ describe Client do
   
   it "should find assets" do
     mock_http(:get, %r|^/drops/mydrop/assets/\?api_key=43myapikey13&token=93mydroptoken97&version=1.0&format=json|, @api_response)
-    Client::Mapper.stub!(:map_assets).with(@mydrop, @api_response_body).and_return([@asset])
-    Client.instance.find_assets(@mydrop).should == [@asset]
+    Client::Mapper.stub!(:map_assets).with(@mydrop, @api_response_body).and_return([@file_asset])
+    Client.instance.find_assets(@mydrop).should == [@file_asset]
   end
   
   it "should find comments" do
     mock_http(:get, %r|^/drops/mydrop/assets/some-video/comments/\?api_key=43myapikey13&token=93mydroptoken97&version=1.0&format=json|, @api_response)
-    Client::Mapper.stub!(:map_comments).with(@asset, @api_response_body).and_return([@comment])
-    Client.instance.find_comments(@asset).should == [@comment]
+    Client::Mapper.stub!(:map_comments).with(@file_asset, @api_response_body).and_return([@comment])
+    Client.instance.find_comments(@file_asset).should == [@comment]
   end
   
   it "should save drops" do
@@ -128,16 +135,56 @@ describe Client do
                                      :format             => "json",
                                      :version            => "1.0")
     
-    Client::Mapper.stub!(:map_drops).with(@api_response_body).and_return(@mydrop)
-    Client.instance.save_drop(@mydrop).should == @mydrop
+    new_drop = stub(Drop)
+    Client::Mapper.stub!(:map_drops).with(@api_response_body).and_return(new_drop)
+    Client.instance.save_drop(@mydrop).should == new_drop
   end
   
+  it "should save note assets" do
+    @note.stub!(:title    => "Just a Note",
+                :contents => "This is just to say")
+    
+    mock_http(:put, "/drops/mydrop/assets/a-note", @api_response,
+                                                   :title       => "Just a Note",
+                                                   :description => nil,
+                                                   :url         => nil,
+                                                   :contents    => "This is just to say",
+                                                   :token       => "93mydroptoken97",
+                                                   :api_key     => "43myapikey13",
+                                                   :format      => "json",
+                                                   :version     => "1.0")
+    
+    new_note = stub_asset
+    Client::Mapper.stub!(:map_assets).with(@mydrop, @api_response_body).and_return(new_note)
+    Client.instance.save_asset(@note).should == new_note
+  end
+
+  it "should save link assets" do
+    @link.stub!(:url         => "http://drop.io/",
+                :title       => "Drop.io",
+                :description => "An awesome sharing site.")
+    
+    mock_http(:put, "/drops/mydrop/assets/a-link", @api_response,
+                                                   :title       => "Drop.io",
+                                                   :description => "An awesome sharing site.",
+                                                   :url         => "http://drop.io/",
+                                                   :contents    => nil,
+                                                   :token       => "93mydroptoken97",
+                                                   :api_key     => "43myapikey13",
+                                                   :format      => "json",
+                                                   :version     => "1.0")
+    
+    new_link = stub_asset
+    Client::Mapper.stub!(:map_assets).with(@mydrop, @api_response_body).and_return(new_link)
+    Client.instance.save_asset(@link).should == new_link
+  end
+
   it "should save file assets" do
-    @asset.stub!(:title       => "Snowboarding in March",
+    @file_asset.stub!(:title       => "Snowboarding in March",
                  :description => "This was really fun.")
     
     mock_http(:put, "/drops/mydrop/assets/some-video", @api_response,
-                                                       :title       =>  "Snowboarding in March",
+                                                       :title       => "Snowboarding in March",
                                                        :description => "This was really fun.",
                                                        :url         => nil,
                                                        :contents    => nil,
@@ -146,7 +193,8 @@ describe Client do
                                                        :format      => "json",
                                                        :version     => "1.0")
     
-    Client::Mapper.stub!(:map_assets).with(@mydrop, @api_response_body).and_return(@asset)
-    Client.instance.save_asset(@asset).should == @asset
+    new_file_asset = stub_asset
+    Client::Mapper.stub!(:map_assets).with(@mydrop, @api_response_body).and_return(new_file_asset)
+    Client.instance.save_asset(@file_asset).should == new_file_asset
   end
 end
