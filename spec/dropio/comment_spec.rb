2fae22d608fe674226e98f9d1cc900e93d86556a
Jake Good <jake@dropio.com>
2009-09-15 17:17:29 -0500

    Fixed more tests

--- spec/dropio/comment_spec.rb
+++ spec/dropio/comment_spec.rb
@@ -5,6 +5,9 @@ describe Comment do
     @comment = Comment.new
     @client = stub(Client)
     Dropio::Resource.stub!(:client).and_return(@client)
+    
+    @client.stub!(:update_comment).and_return(@comment)
+    @client.stub!(:delete_comment).and_return(@comment)
   end
   
   it "should have the attributes of an Comment" do
@@ -12,12 +15,12 @@ describe Comment do
   end
   
   it "should save itself" do
-    @client.should_receive(:save_comment).with(@comment)
+    @client.should_receive(:update_comment).with(@comment)
     @comment.save
   end
   
   it "should destroy itself" do
-    @client.should_receive(:destroy_comment).with(@comment)
+    @client.should_receive(:delete_comment).with(@comment)
     @comment.destroy
   end
 end
