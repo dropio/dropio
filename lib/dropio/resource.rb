e408f668f98bec55989fc160c6d08ef1dee4fbed
Kunal Shah <kunal@dropio.com>
2010-09-17 22:24:11 -0400

    fix, switch to checking for the setter

--- lib/dropio/resource.rb
+++ lib/dropio/resource.rb
@@ -2,7 +2,7 @@ class Dropio::Resource
   
   def initialize(params = {})
     params.each do |key,val|
-      self.send("#{key}=", val) if self.respond_to? key
+      self.send("#{key}=", val) if self.respond_to?("#{key}=")
     end
   end
 
